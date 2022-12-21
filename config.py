4fd4ffe6f03451e3f16ba9ad6afff744261fc543
Justin Schembri <jus.schembri@gmail.com>
2022-12-21 23:38:30 +0100

    Created a config file for directory definitions
    Config file contains variables of various paths needed for the project.

---
+++ config.py
@@ -0,0 +1,10 @@
+# Configuration settings for nlp4ged.
+from pathlib import Path
+## DATAPATHS.
+
+ROOT_PATH = Path(__file__).parent
+ROOT_DIR = ROOT_PATH.resolve()
+
+DATA_PATH = ROOT_PATH / 'data'
+DATA_DIR = DATA_PATH.resolve()
+
